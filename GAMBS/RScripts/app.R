1a492797e0eed5bec8ee34cdc7aebfd177b1025f
sk6108 <Sarah.klein@teradata.com>
2017-07-20 12:19:17 +0100

    bugfix

--- GAMBS/RScripts/app.R
+++ GAMBS/RScripts/app.R
@@ -10,12 +10,15 @@ library(scales)
 library(RColorBrewer)
 library (DT)
 library(plotly)
+library(ggplot2)
+library(plyr)
 #library(RQuantLib)
 #library(rPython)
 library("colorspace")
 
 
 load("./drugs.RData")
+load("./murder.RData")
 load("./locations.RData")
 load("./pred_drug.RData")
 load("./pred_murder.RData")
@@ -31,10 +34,22 @@ plot_map <- function(category){
                                                 popup = paste(loc$region,"<br/> Poverty rate:",round(loc$poverty,1)),
                                                 radius = as.numeric(paste(loc$population))/200000,
                                                 fillOpacity = 0.8
-                                                ) 
+  ) 
 }
 
+drugsNORM <- split.data.frame(drugsNORM_TOT,drugsNORM_TOT$REGION)
+ind <- sapply(seq(1:length(drugNORM)), function(x) cbind(drugNORM[[x]]$RATE))
+ind <- cbind(drugNORM[[1]]$YEAR,ind)
+colnames(ind) <- c("YEAR",names(drugsNORM))
+drugsNORM <- ind %>% as.data.frame
+rm(ind)
 
+murder_regions <- split.data.frame(murderReg_TOT,murderReg_TOT$REGION)
+ind <- sapply(seq(1:length(murder_regions)), function(x) cbind(murder_regions[[x]]$RATE))
+ind <- cbind(murder_regions[[1]]$YEAR,ind)
+colnames(ind) <- c("Year",names(murder_regions))
+murder_regions <- ind %>% as.data.frame
+rm(ind)
 
 server <- function(input, output,session) {
   ##############################################################################
@@ -52,11 +67,11 @@ server <- function(input, output,session) {
   
   
   observeEvent(input$give_murder,{
-  output$plotmurder <- renderPlot({
-    print(input$murderregion)
-    p<- ggplot() +  geom_line(aes(x= murder_regions$Year,y=murder_regions[[paste(input$murderregion)]]),size=1.2,color = "black") + xlab("Year") + ylab("Murder rate")   +  geom_line(aes(x= pred_murder$year,y=pred_murder[[paste(input$murderregion)]],size=2),color = "orange") 
-    p
-  })
+    output$plotmurder <- renderPlot({
+      print(input$murderregion)
+      p<- ggplot() +  geom_line(aes(x= murder_regions$Year,y=murder_regions[[paste(input$murderregion)]]),size=1.2,color = "black") + xlab("Year") + ylab("Murder rate")   +  geom_line(aes(x= pred_murder$year,y=pred_murder[[paste(input$murderregion)]],size=2),color = "orange") 
+      p
+    })
   })
   
   observeEvent(input$give_drugs,{
@@ -76,7 +91,7 @@ server <- function(input, output,session) {
   output$choseregionMurder <- renderUI({
     choseregionMurder <- selectizeInput('murderregion',"Select a region... ",choices  = c("...",loc$region))
   })
-
+  
   
 }
 
@@ -95,63 +110,63 @@ sidebar <- dashboardSidebar(
 body <- dashboardBody(
   # tabItems correspond to entries in the sidebar
   tabItems(
-  tabItem(tabName = 'murder',
+    tabItem(tabName = 'murder',
             mainPanel(
               fluidRow(width=12,height = 12,
-                HTML("<h2>Murder\n</h2><br/>
-                      <h4><b>Circle size:</b> Population in the area, <b>Circle color:</b> Average Murder rate (1960-2014)"),
-                leafletOutput('plot_map_murder',height = 700,width = 1200 ),
-                HTML("<br/> <h4>"),
-                #plotOutput("plotmurder",height = 500,width = 1200 ),
-                box(uiOutput('choseregionMurder'),
-                    actionButton('give_murder',"Calculate Prediction"),width = 3
+                       HTML("<h2>Murder\n</h2><br/>
+                            <h4><b>Circle size:</b> Population in the area, <b>Circle color:</b> Average Murder rate (1960-2014)"),
+                       leafletOutput('plot_map_murder',height = 700,width = 1200 ),
+                       HTML("<br/> <h4>"),
+                       #plotOutput("plotmurder",height = 500,width = 1200 ),
+                       box(uiOutput('choseregionMurder'),
+                           actionButton('give_murder',"Calculate Prediction"),width = 3
                        ),
-                plotOutput("plotmurder",height = 500,width = 800 )
+                       plotOutput("plotmurder",height = 500,width = 800 )
+                       )
+            )
+    ),
+    tabItem(tabName = 'drugs',
+            mainPanel(
+              fluidRow(
+                HTML("<h2>Drug Abuse\n</h2><br/><h4><b>Circle size:</b> Population in the area, <b>Circle color:</b> Average Drug abuse rate (2004-2011)"),
+                leafletOutput('plot_map_drugs',height = 700,width = 1200 ),
+                HTML("<br/><h4> "),
+                #plotOutput("plotdrugs",height = 500,width = 1200 )
+                box(uiOutput('choseregionDrugs'),
+                    actionButton('give_drugs',"Calculate Prediction"), width=3
+                ),
+                plotOutput("plotdrugs",height = 500,width = 800 )
+                
               )
             )
-  ),
-  tabItem(tabName = 'drugs',
-          mainPanel(
-            fluidRow(
-              HTML("<h2>Drug Abuse\n</h2><br/><h4><b>Circle size:</b> Population in the area, <b>Circle color:</b> Average Drug abuse rate (2004-2011)"),
-              leafletOutput('plot_map_drugs',height = 700,width = 1200 ),
-              HTML("<br/><h4> "),
-              #plotOutput("plotdrugs",height = 500,width = 1200 )
-              box(uiOutput('choseregionDrugs'),
-                  actionButton('give_drugs',"Calculate Prediction"), width=3
-                              ),
-              plotOutput("plotdrugs",height = 500,width = 800 )
-              
-            )
-          )
-  ),
- tabItem(tabName = "intro",
-         titlePanel("Introduction"),
-          mainPanel(
-            fluidRow(
+    ),
+    tabItem(tabName = "intro",
+            titlePanel("Introduction"),
+            mainPanel(
               fluidRow(
-                box(width=12,
-                    h2("Problem Statement", style="color:orange"),
-                    h3("Build a analytic solution that predicts murder and crime rates for several metropolitan statistical areas (MSA) in the US.")
-                ),
-                box(width=12,
-                    h2("Analytic Approach", style="color:orange"),
-                    h3("Stochastic propagation processes have been employed to predict the crime and drug abuse rate in the different MSAs for the next 5 years.
-                       The processes was trained by the historical data provided by the Federal Bureau of Investigation (FBI) and the Drug Abuse Warning Network (DAWN).")
-                    ),
-                box(width=12,
-                    h2("Business Benefit",style="color:orange"),
-                    h3("Violent crime is of immense cost throughout the United States and was estimated $179 billion in government expenditures on police protection, judicial and legal activities, 
-                        and corrections (US Department of Justice). Further, the economic cost of drug abuse in the United States was estimated at $180.9 billion in 2002 (Office of National Drug Control Policy).
-
-                       With the knowledge of future commited violent crime and the abuse of drugs these cost can be reduced dramatically.")
-                    )
-                )
-                )
+                fluidRow(
+                  box(width=12,
+                      h2("Problem Statement", style="color:orange"),
+                      h3("Build a analytic solution that predicts murder and crime rates for several metropolitan statistical areas (MSA) in the US.")
+                  ),
+                  box(width=12,
+                      h2("Analytic Approach", style="color:orange"),
+                      h3("Stochastic propagation processes have been employed to predict the crime and drug abuse rate in the different MSAs for the next 5 years.
+                         The processes was trained by the historical data provided by the Federal Bureau of Investigation (FBI) and the Drug Abuse Warning Network (DAWN).")
+                      ),
+                  box(width=12,
+                      h2("Business Benefit",style="color:orange"),
+                      h3("Violent crime is of immense cost throughout the United States and was estimated $179 billion in government expenditures on police protection, judicial and legal activities, 
+                         and corrections (US Department of Justice). Further, the economic cost of drug abuse in the United States was estimated at $180.9 billion in 2002 (Office of National Drug Control Policy).
+                         
+                         With the knowledge of future commited violent crime and the abuse of drugs these cost can be reduced dramatically.")
+                      )
+                      )
+                      )
+                  )
+                  )
             )
-          )
- )
-)
+            )
 
 # tb_images <- system.file("extdata", "tb_images", package = "TBdesign")
 # 
