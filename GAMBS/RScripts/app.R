b5e9525f1a50e3221a3a72c99b4c6c2b89e054c6
sk6108 <Sarah.klein@teradata.com>
2017-07-20 11:16:10 +0100

    Updated intro-tab, added legends to the maps.

--- GAMBS/RScripts/app.R
+++ GAMBS/RScripts/app.R
@@ -28,7 +28,7 @@ plot_map <- function(category){
                                                 fill = TRUE,
                                                 color = 0,
                                                 fillColor = colors[cols],
-                                                popup = loc$region,
+                                                popup = paste(loc$region,"<br/> Poverty rate:",round(loc$poverty,1)),
                                                 radius = as.numeric(paste(loc$population))/200000,
                                                 fillOpacity = 0.8
                                                 ) 
@@ -76,8 +76,7 @@ server <- function(input, output,session) {
   output$choseregionMurder <- renderUI({
     choseregionMurder <- selectizeInput('murderregion',"Select a region... ",choices  = c("...",loc$region))
   })
-  
-  
+
   
 }
 
@@ -99,7 +98,8 @@ body <- dashboardBody(
   tabItem(tabName = 'murder',
             mainPanel(
               fluidRow(width=12,height = 12,
-                (HTML("<h2>Murder\n<p>")),
+                HTML("<h2>Murder\n</h2><br/>
+                      <h4><b>Circle size:</b> Population in the area, <b>Circle color:</b> Average Murder rate (1960-2014)"),
                 leafletOutput('plot_map_murder',height = 700,width = 1200 ),
                 HTML("<br/> <h4>"),
                 #plotOutput("plotmurder",height = 500,width = 1200 ),
@@ -113,7 +113,7 @@ body <- dashboardBody(
   tabItem(tabName = 'drugs',
           mainPanel(
             fluidRow(
-              (HTML("<h2>Drug Abuse\n<p>")),
+              HTML("<h2>Drug Abuse\n</h2><br/><h4><b>Circle size:</b> Population in the area, <b>Circle color:</b> Average Drug abuse rate (2004-2011)"),
               leafletOutput('plot_map_drugs',height = 700,width = 1200 ),
               HTML("<br/><h4> "),
               #plotOutput("plotdrugs",height = 500,width = 1200 )
@@ -129,17 +129,39 @@ body <- dashboardBody(
          titlePanel("Introduction"),
           mainPanel(
             fluidRow(
-              
-                htmlOutput('introText')
+              fluidRow(
+                box(width=12,
+                    h2("Problem Statement", style="color:orange"),
+                    h3("Build a analytic solution that predicts murder and crime rates for several metropolitan statistical areas (MSA) in the US.")
+                ),
+                box(width=12,
+                    h2("Analytic Approach", style="color:orange"),
+                    h3("Stochastic propagation processes have been employed to predict the crime and drug abuse rate in the different MSAs for the next 5 years.
+                       The processes was trained by the historical data provided by the Federal Bureau of Investigation (FBI) and the Drug Abuse Warning Network (DAWN).")
+                    ),
+                box(width=12,
+                    h2("Business Benefit",style="color:orange"),
+                    h3("Violent crime is of immense cost throughout the United States and was estimated $179 billion in government expenditures on police protection, judicial and legal activities, 
+                        and corrections (US Department of Justice). Further, the economic cost of drug abuse in the United States was estimated at $180.9 billion in 2002 (Office of National Drug Control Policy).
+
+                       With the knowledge of future commited violent crime and the abuse of drugs these cost can be reduced dramatically.")
+                    )
+                )
+                )
             )
-          )
           )
  )
 )
 
+# tb_images <- system.file("extdata", "tb_images", package = "TBdesign")
+# 
+# div(style = "position: fixed; bottom: 35px; left: 35px;",
+#     img(src = 'tb_images/tb_logo.png', width = 197)
+# )
 
 
 ui <- dashboardPage(dashboardHeader(title = 'InSights'),
+                    #TBdesign::td_colors$primary["orange"],
                     skin = 'yellow',
                     sidebar = sidebar, 
                     body = body)
